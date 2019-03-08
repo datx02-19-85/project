98bf74a9a8c14170d286c4a643c1e3979edce5f9
mattssonj <joamatt@student.chalmers.se>
2019-03-08 16:49:35 +0100

    add graphical network to use graphical local ganache.

--- truffle-config.js
+++ truffle-config.js
@@ -55,6 +55,12 @@ module.exports = {
       gasPrice: gasPrice
     },
 
+    graphical: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "*"
+    }
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
