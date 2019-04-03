ccbbefbbf38f9f60494a59b649cc7c7a3e14811b
Aron Sandstedt <aron.sandstedt@live.se>
2019-04-03 15:40:04 +0200

    add functionality for copy button

--- client/src/screens/RenderID.js
+++ client/src/screens/RenderID.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
 import Hash from 'object-hash';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import FlipFlap from '../components/FlipFlap';
 import Button from '../components/Button';
 
 class RenderID extends Component {
   constructor(props) {
     super(props);
+    const { drizzle } = this.props;
+    const contract = drizzle.contracts.Voting;
+
     this.state = {
       i: 'nothing',
-      copy: true
+      copy: true,
+      contract,
+      copied: false
     };
   }
 
+  async getNVoters() {
+    const { state } = this;
+    const n = await state.contract.getNumberOfVotes().call();
+    return n;
+  }
+
   genHash = () => {
+    const hash = Hash(/* this.getNVoters() */ 123);
+    this.addVoter(hash);
     this.setState({
-      i: Hash(123),
+      i: hash,
       copy: false
     });
   };
 
+  onCopy = () => {
+    this.setState({
+      copied: true
+    });
+  };
+
+  async addVoter(hash) {
+    const { state } = this;
+    await state.contract.addVoter(hash).call();
+  }
+
   render() {
-    // const { drizzle, drizzleState } = this.props;
-    const { state, genHash } = this;
+    const { state, onCopy, genHash } = this;
 
     return (
       <div
@@ -32,12 +56,18 @@ class RenderID extends Component {
           alignItems: 'center'
         }}
       >
+        <h1>Generate Voting ID</h1>
         <div>
           <FlipFlap id={state.i} />
         </div>
         <div>
           <Button name="Generate" color="danger" onClick={genHash} />
-          <Button name="Copy Hash" color="warning" disabled={state.copy} />
+          <CopyToClipboard onCopy={onCopy} text={state.i}>
+            <Button name="Copy Hash" color="warning" disabled={state.copy} />
+          </CopyToClipboard>
+        </div>
+        <div>
+          {state.copied ? <span style={{ color: 'red' }}>Copied</span> : null}
         </div>
       </div>
     );
